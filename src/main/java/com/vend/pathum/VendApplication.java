d2e61a0f8492730f661c9486ad16232dbf5c1409
pathum83 <pathum83@gmail.com>
2019-03-24 15:52:34 +1300

    Add tomcat deployable capability.

--- src/main/java/com/vend/pathum/VendApplication.java
+++ src/main/java/com/vend/pathum/VendApplication.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendApplication {
+public class VendApplication extends SpringBootServletInitializer {
 
 	private static final Logger logger = LoggerFactory.getLogger(VendApplication.class);
 
@@ -18,4 +20,8 @@ public class VendApplication {
 		SpringApplication.run(VendApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VendApplication.class);
+	}
 }
